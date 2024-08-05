43be0cf6327d6a3bd7d7bcaceafa1d72de185bd2
hobbyrum <9680372+hobbyrum@users.noreply.github.com>
2024-08-05 15:06:07 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,8 @@ export default function Home() {
   const [query, setQuery] = useState('');
   const [theme, setTheme] = useState('light');
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; // Adjust this number to set items per page
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const urlPrefix = "https://www.flashback.org/";
 
   useEffect(() => {
     fetch('/data.json')
@@ -22,19 +23,42 @@ export default function Home() {
     document.documentElement.classList.toggle('dark', theme === 'dark');
   }, [theme]);
 
-  return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 dark:text-white p-4">
-      <div className="mb-4 flex justify-between items-center">
-        <Search query={query} setQuery={setQuery} />
-        <ThemeSwitcher theme={theme} setTheme={setTheme} />
-      </div>
-      <Table
-        data={data}
-        query={query}
-        currentPage={currentPage}
-        itemsPerPage={itemsPerPage}
-        setCurrentPage={setCurrentPage}
-      />
+  const handleItemsPerPageChange = (e) => {
+    setItemsPerPage(Number(e.target.value));
+    setCurrentPage(1); // Reset to first page when items per page changes
+  }
+
+  return ( // return-main
+    <div>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 dark:text-white p-4"> { /* div-main */ }
+        <div className="sticky mb-4 flex justify-between items-center">
+          <Search query={query} setQuery={setQuery} />
+          <ThemeSwitcher theme={theme} setTheme={setTheme} />
+        </div>
+        <Table
+          data={data}
+          query={query}
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          setCurrentPage={setCurrentPage}
+          urlPrefix={urlPrefix}
+        />
+        <div> { /* div-label */ }
+          <label htmlFor='itemsPerPage' className='mr-2'>Rader per sida:</label>
+          <select
+            id="itemsPerPage"
+            value={itemsPerPage}
+            onChange={handleItemsPerPageChange}
+            className='rounded p-2 bg-white dark:bg-gray-700 dark:text-white'
+          >
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+
+          </select>
+        </div> { /* end of div-label */ }
+      </div> { /* end of div-main */}
     </div>
-  );
-}
+  ); // End of return-main
+} // End of Home
